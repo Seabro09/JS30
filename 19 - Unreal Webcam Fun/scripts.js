8b656b69476d88d60511c4cff800cba757c3c715
Seabro09 <paulwarrenseabrook@gmail.com>
2022-10-25 11:44:39 -0400

    Working on Webcam Fun

--- 19 - Unreal Webcam Fun/scripts.js
+++ 19 - Unreal Webcam Fun/scripts.js
@@ -6,6 +6,12 @@ const snap = document.querySelector(".snap");
 
 function getVideo() {
   // prompts the user for permission to use a media input (like a video) and produces a MediaStream with tracks containing the requested media.
-  // with a promise, we have a .then
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then();
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    // with a promise, we have a .then
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+    });
 }
+
+getVideo();
